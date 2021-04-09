94870314606caba6af21bb65e565b478e92d73ba
Sixtten610 <60339623+Sixtten610@users.noreply.github.com>
2021-04-09 16:09:59 +0200

    widgets added

--- Asteroids/TitleScreen.cs
+++ Asteroids/TitleScreen.cs
@@ -7,10 +7,12 @@ namespace Asteroids
     public class TitleScreen : ObjectScreen
     {
         // BUTTONS ###########################################
-
-        // TEXT ##############################################
-
-        // COLORS ############################################
+        Button singlePlayer = new Button(45, 300, 35, Color.WHITE, Color.WHITE, Color.LIGHTGRAY, 350, 400, "SINGLEPLAYER", 16, 6);
+        Button multiPlayer = new Button(45, 300, 35, Color.WHITE, Color.WHITE, Color.LIGHTGRAY, 350, 450, "MULTIPLAYER", 26, 6);
+        Button difficulty = new Button(45, 300, 35, Color.WHITE, Color.WHITE, Color.LIGHTGRAY, 350, 500, "DIFFICULTY", 42, 6);
+        Button settings = new Button(45, 300, 35, Color.WHITE, Color.WHITE, Color.LIGHTGRAY, 350, 550, "SETTINGS", 56, 6);
+        Button quit = new Button(45, 300, 35, Color.WHITE, Color.WHITE, Color.LIGHTGRAY, 350, 600, "QUIT", 106, 6);
+        
 
 
         public override void Update()
@@ -20,9 +22,11 @@ namespace Asteroids
 
         public override void Draw()
         {
-            Raylib.DrawText("ASTEROIDS X", )
+            Raylib.DrawText("ASTEROIDS II", 325, 250, 50, Color.WHITE);
 
+            Button.DrawAll();
 
+            if ()
         }
         
     }
