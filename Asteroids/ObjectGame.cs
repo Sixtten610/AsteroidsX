feeb26e62cc3c574a7054bb3ccddd5e5e5fd0506
Sixtten610 <60339623+Sixtten610@users.noreply.github.com>
2021-04-12 12:53:23 +0200

    fixes

--- Asteroids/ObjectGame.cs
+++ Asteroids/ObjectGame.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System;
 using Raylib_cs;
 
@@ -5,15 +6,6 @@ namespace Asteroids
 {
     public class ObjectGame
     {
-        public ObjectGame()
-        {
-            CreatePlayers();
-        }
-
-        protected virtual void CreatePlayers()
-        {
-            RegularSpaceship regularSpaceship = new RegularSpaceship(KeyboardKey.KEY_W, KeyboardKey.KEY_S, KeyboardKey.KEY_A, KeyboardKey.KEY_D, KeyboardKey.KEY_LEFT, KeyboardKey.KEY_RIGHT, KeyboardKey.KEY_SPACE);
-        }
 
         public virtual void Draw()
         {
--- Asteroids/SinglePlayerGame.cs
+++ Asteroids/SinglePlayerGame.cs
@@ -1,9 +1,25 @@
 using System;
+using Raylib_cs;
+using System.Collections.Generic;
 
 namespace Asteroids
 {
     public class SinglePlayerGame : ObjectGame
     {
-        
+        RegularSpaceship regularSpaceship;
+        public SinglePlayerGame()
+        {
+            regularSpaceship = new RegularSpaceship(KeyboardKey.KEY_W, KeyboardKey.KEY_S, KeyboardKey.KEY_A, KeyboardKey.KEY_D, KeyboardKey.KEY_LEFT, KeyboardKey.KEY_RIGHT, KeyboardKey.KEY_SPACE);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (regularSpaceship.Shoot())
+            {
+
+            }
+        }
     }
 }
